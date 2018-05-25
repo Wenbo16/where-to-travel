5fbd8f761c61409ac1ae51ebf16d9e0f2cc16780
wenbo-feng <wenbo_feng@apple.com>
2018-05-25 14:59:44 -0700

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+			<!-- slides -->
+			<swiper-slide v-for="item of swiperList" :key="item.id">
+				<img class="swiper-img" :src="item.imgUrl" >
+			</swiper-slide>
+			<!-- Optional controls -->
+			<div class="swiper-pagination"  slot="pagination"></div>
+		</swiper>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeSwiper',
+	data () {
+		return {
+			swiperOption: {
+				pagination: '.swiper-pagination',
+				loop: true
+			},
+			swiperList: [{
+				id: '0001',
+				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/5c/969bfa7af00a2702.jpg_750x200_b99b8e07.jpg'
+			},
+			{
+				id: '0002',
+				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1709/39/b98fdb3ee7478e02.jpg_750x200_be64b756.jpg'
+			},
+			{
+				id: '0003',
+				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
+			}]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	.wrapper >>> .swiper-pagination-bullet-active
+		background: #fff
+	.wrapper
+		width: 100%
+		overflow: hidden
+		height: 0
+		padding-bottom: 31.25%
+		.swiper-img
+			width: 100%
+</style>
